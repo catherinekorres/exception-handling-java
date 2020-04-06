f3ae8fbf927f27eac0cd53c5e635eaa0fa21dbc1
Catherine Korres <cathy.korres@gmail.com>
2020-04-06 16:59:37 -0300

    Add ThrowsExample.java

---
+++ ThrowsExample.java
@@ -0,0 +1,15 @@
+public class ThrowsExample {
+
+  public static void main(String[ ] args) throws RuntimeException  {
+      int x = 10;
+      int y = 2;
+
+      System.out.println(x/y);
+
+      y = 0;
+
+      System.out.println(x/y);
+
+  }
+
+}
