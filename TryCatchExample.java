10bc698a3c64a0aabe1ae458af4446d5b965ac32
Catherine Korres <cathy.korres@gmail.com>
2020-04-06 16:47:23 -0300

    Add TryCatchExample.java

---
+++ TryCatchExample.java
@@ -0,0 +1,19 @@
+public class TryCatchExample {
+
+  public static void main(String[ ] args) {
+    try {
+      int x = 10;
+      int y = 2;
+
+      System.out.println(x/y);
+
+      y = 0;
+
+      System.out.println(x/y);
+
+    } catch (Exception e) {
+      System.out.println("Algo deu errado. Nao foi possivel executar o codigo.");
+    }
+  }
+
+}
